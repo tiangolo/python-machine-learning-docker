8d6a4d4063d9a8601268a5b07cdc0c147bc24a35
Sebastián Ramírez <tiangolo@gmail.com>
2019-03-05 19:35:26 +0400

    :white_check_mark: Call python directly, not through echo

--- conda-python3.6/start.sh
+++ conda-python3.6/start.sh
@@ -3,4 +3,5 @@ set -e
 
 echo "conda version: $(conda --version)"
 
-echo "python version: $(python -c 'import sys; print("{}.{}".format(sys.version_info.major, sys.version_info.minor))')"
+python -c 'import sys; print("python version: {}.{}".format(sys.version_info.major, sys.version_info.minor))'
+
