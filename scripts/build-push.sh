73a71c4eb4323ef4d0d46e148d0f9aafcb9d4c09
Sebastián Ramírez <tiangolo@gmail.com>
2019-03-05 16:44:45 +0400

    :rocket: Add deploying scripts

---
+++ scripts/build-push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker-compose -f docker-compose.build.stage01.yml build
+
+docker-compose -f docker-compose.build.stage01.yml push
+
+# docker-compose -f docker-compose.build.stage02.yml build
+
+# docker-compose -f docker-compose.build.stage02.yml push
---
+++ scripts/trigger-children.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+echo "No children yet"
+
+# body='{
+# "request": {
+# "branch":"master"
+# }}'
+
+# for child in tiangolo%2Fuvicorn-gunicorn-fastapi-docker tiangolo%2Fuvicorn-gunicorn-starlette-docker; do
+#    curl -s -X POST \
+#       -H "Content-Type: application/json" \
+#       -H "Accept: application/json" \
+#       -H "Travis-API-Version: 3" \
+#       -H "Authorization: token $TRAVIS_TOKEN" \
+#       -d "$body" \
+#       https://api.travis-ci.org/repo/$child/requests
+# done
